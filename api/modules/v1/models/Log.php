37d2b7f10ec136d73419f0560c66dfb7309d37f2
Sam <34n@163.com>
2016-11-16 16:38:16 +0800

    box log

---
+++ api/modules/v1/models/Log.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace api\modules\v1\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+use yii\data\ActiveDataProvider;
+
+/**
+ * This is the model class for table "log".
+ *
+ * @property integer $id
+ * @property string $model
+ * @property integer $parent_id
+ * @property integer $rel_id
+ * @property integer $user_id
+ * @property string $action
+ * @property string $message
+ * @property integer $created_at
+ * @property string $created_by
+ */
+class Log extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'log';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['parent_id', 'rel_id', 'user_id'], 'required'],
+            [['parent_id', 'rel_id', 'user_id', 'created_at'], 'integer'],
+            [['model'], 'string', 'max' => 20],
+            [['action'], 'string', 'max' => 10],
+            [['message'], 'string', 'max' => 100],
+            [['created_by'], 'string', 'max' => 45],
+        ];
+    }
+
+    public function fields()
+    {
+        return [
+            'id' => $this->rel_id,
+            'model',
+            'action',
+            'message',
+            'created_by',
+            'created_at',
+        ];
+    }
+
+
+    public function addLog($parent_id,$rel_id,$user_id,$model_name,$action,$message,$created_by)
+    {
+        $this->parent_id    = $parent_id;
+        $this->rel_id       = $rel_id;
+        $this->user_id      = $user_id;
+        $this->model        = $model_name;
+        $this->action       = $action;
+        $this->message      = $message;
+        $this->created_by   = $created_by;
+        $this->save();
+
+    }
+}
