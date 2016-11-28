af6e1980714228a0abc976ef751821cd609dc3be
Sam <34n@163.com>
2016-11-28 11:30:09 +0800

    Images Sawgger

--- api/modules/v1/controllers/ImagesController.php
+++ api/modules/v1/controllers/ImagesController.php
@@ -23,6 +23,9 @@ class ImagesController extends ActiveController
         $behaviors['authenticator'] = ['class' => QueryParamAuth::className()];
         $behaviors['contentNegotiator']['formats'] = ['application/json' => Response::FORMAT_JSON];
         $this->userinfo = isset($_GET['access-token'])?User::getUserInfo($_GET['access-token']):'';
+        if(!empty($this->userinfo)){
+            $this->userinfo->nickname = ($this->userinfo->nickname!="")?$this->userinfo->nickname:$this->userinfo->_nickname;
+        }
         return $behaviors;
     }
 
@@ -49,33 +52,160 @@ class ImagesController extends ActiveController
         return $actions;
     }
 
-
+    /**
+     *
+     *	@SWG\Post(
+     * 		path="/images/upload?access-token={access_token}",
+     * 		tags={"Images"},
+     * 		operationId="uploadImg",
+     * 		summary="上传图片",
+     *      @SWG\Parameter(
+     * 			name="access_token",
+     * 			in="path",
+     * 			required=true,
+     *          type="string",
+     * 			description="访问令牌",
+     *		),
+     * 		@SWG\Parameter(
+     * 			name="project_id",
+     * 			in="formData",
+     * 			required=true,
+     * 			type="integer",
+     * 			description="项目ID",
+     * 		),
+     * 		@SWG\Parameter(
+     * 			name="file",
+     * 			in="formData",
+     * 			required=true,
+     * 			type="file",
+     * 			description="要上传的图片文件",
+     * 		),
+     *      @SWG\Parameter(
+     * 			name="model_name",
+     * 			in="formData",
+     * 			required=true,
+     * 			type="string",
+     * 			description="模型:avatar,box,item",
+     * 		),
+     * 		@SWG\Parameter(
+     * 			name="rel_id",
+     * 			in="formData",
+     * 			required=false,
+     * 			type="integer",
+     * 			description="关联模型的ID,比如上传盒子图片,则关联这个盒子的ID,一般在编辑状态下才会使用",
+     * 		),
+     * 		@SWG\Response(
+     * 			response=200,
+     * 			description="成功",
+     * 		),
+     * 	)
+     */
     public function actionUpload()
     {
         $model = new Images();
         $model_name = (isset($_POST['model_name']))?$_POST['model_name']:'item';
-        $data = $model->upload($model_name,$this->userinfo->user_id);
+        $data = $model->upload($model_name,$this->userinfo->user_id,$this->userinfo->nickname);
         return $data;
     }
 
 
+    /**
+     *
+     *	@SWG\Post(
+     * 		path="/images/upload-avatar?access-token={access_token}",
+     * 		tags={"Images"},
+     * 		operationId="uploadAvatar",
+     * 		summary="上传头像",
+     *      @SWG\Parameter(
+     * 			name="access_token",
+     * 			in="path",
+     * 			required=true,
+     *          type="string",
+     * 			description="访问令牌",
+     *		),
+     * 		@SWG\Parameter(
+     * 			name="file",
+     * 			in="formData",
+     * 			required=true,
+     * 			type="file",
+     * 			description="要上传的图片文件",
+     * 		),
+     *      @SWG\Parameter(
+     * 			name="type",
+     * 			in="formData",
+     * 			required=false,
+     * 			type="string",
+     * 			description="微信获取头像时,不用提交type参数,用户在账户内自行上传头像时type=update",
+     * 		),
+     * 		@SWG\Response(
+     * 			response=200,
+     * 			description="成功",
+     * 		),
+     * 	)
+     */
     public function actionUploadAvatar()
     {
         $model = new Images();
-        $data = $model->uploadAvatar($this->userinfo->user_id);
+        $data = $model->uploadAvatar($this->userinfo->user_id,$this->userinfo->nickname);
         return $data;
     }
 
 
+    /**
+     *
+     *	@SWG\Delete(
+     * 		path="/images/{id}?access-token={access_token}",
+     * 		tags={"Images"},
+     * 		operationId="deleteImg",
+     * 		summary="删除图片",
+     * 		@SWG\Parameter(
+     * 			name="id",
+     * 			in="path",
+     * 			required=true,
+     * 			type="integer",
+     * 			description="盒子ID",
+     * 		),
+     *      @SWG\Parameter(
+     * 			name="access_token",
+     * 			in="path",
+     * 			required=true,
+     *          type="string",
+     * 			description="访问令牌",
+     *		),
+     * 		@SWG\Parameter(
+     * 			name="project_id",
+     * 			in="formData",
+     * 			required=true,
+     * 			type="integer",
+     * 			description="项目ID",
+     * 		),
+     *      @SWG\Parameter(
+     * 			name="model_name",
+     * 			in="formData",
+     * 			required=true,
+     * 			type="string",
+     * 			description="模型:avatar,box,item",
+     * 		),
+     * 		@SWG\Parameter(
+     * 			name="rel_id",
+     * 			in="formData",
+     * 			required=true,
+     * 			type="integer",
+     * 			description="关联模型的ID,比如上传盒子图片,则关联这个盒子的ID,一般在编辑状态下才会使用",
+     * 		),
+     * 		@SWG\Response(
+     * 			response=200,
+     * 			description="成功",
+     * 		),
+     * 	)
+     */
     public function actionDelete($id)
     {
         $model = new Images();
         $params = Yii::$app->request->bodyParams;
         $model_name = (isset($params['model_name']))?$params['model_name']:'item';
-        $data = $model->remove($model_name,$id,$this->userinfo->user_id);
+        $data = $model->remove($model_name,$id,$this->userinfo->user_id,$this->userinfo->nickname);
         return $data;
     }
-
-
 
 }
--- api/modules/v1/models/Images.php
+++ api/modules/v1/models/Images.php
@@ -52,7 +52,10 @@ class Images extends ActiveRecord
         return [
             'img_id',
             'model',
-            'rel_id',
+            'rel_id'=>function(){
+                $data = (empty($this->rel_id))?'':$this->rel_id;
+                return $data;
+            },
             'o_path' => function(){
                 $data = (empty($this->o_path))?'':Yii::$app->params['imgServer'].$this->o_path;
                 return $data;
@@ -74,33 +77,38 @@ class Images extends ActiveRecord
 
 
     //上传盒子和物品图片
-    public function upload($model_name='item',$user_id)
+    public function upload($model_name='item',$user_id,$nickname)
     {
         //检查参数
-        if(!isset($model_name,$_POST['project_id'],$_POST['created_by'])){
+        if(!isset($model_name,$_POST['project_id'],$user_id,$nickname)){
             $data['code']  = 20000;
             return $data;
         }
 
+        //检测是否是更新图片
         if(isset($_POST['project_id'],$_POST['rel_id'])){
             $this->rel_id = $_POST['rel_id'];
             if($model_name == 'item'){
                 $query = Item::findOne(['project_id'=>$_POST['project_id'], 'item_id'=>$this->rel_id]);
+                if(empty($query)) {
+                    $data['code'] = 50001;
+                    return $data;
+                }
                 $parent_id = $query->box_id;
                 $rel_id    = $query->item_id;
             }else{
                 $query = Box::findOne(['project_id'=>$_POST['project_id'], 'box_id'=>$this->rel_id]);
+                if(empty($query)) {
+                    $data['code'] = 50001;
+                    return $data;
+                }
                 $parent_id = $query->project_id;
                 $rel_id    = $query->box_id;
             }
 
-            if(empty($query)){
-                $data['code']  = 50001;
-                return $data;
-            }else{
-                if(isset($query->img->img_id) && $query->img->img_id!=""){
-                    $this->removeImg($query->img->img_id);
-                }
+            //检查图片是否存在,存在则清除
+            if(isset($query->img->img_id) && $query->img->img_id!=""){
+                $this->removeImg($query->img->img_id);
             }
         }
 
@@ -118,7 +126,7 @@ class Images extends ActiveRecord
         //图片库关联
         $this->model     = $model_name;
         $this->project_id= $_POST['project_id'];
-        $this->created_by= $_POST['created_by'];
+        $this->created_by= $nickname;
         $this->o_path    = $file['path'].$file['file'];
         $this->l_path    = $img_l;
         $this->m_path    = $img_m;
@@ -129,7 +137,7 @@ class Images extends ActiveRecord
         //日志
         if(isset($parent_id,$rel_id)){
             $log = new Log();
-            $log->addLog($parent_id,$rel_id,$user_id,$model_name,'img-upload','上传图片',$this->created_by);
+            $log->addLog($parent_id,$rel_id,$user_id,$model_name,'img-upload','上传图片',$nickname);
         }
 
         //返回客户端数据
@@ -141,10 +149,10 @@ class Images extends ActiveRecord
 
 
     //上传头像
-    public function uploadAvatar($user_id)
+    public function uploadAvatar($user_id,$nickname)
     {
         //验证参数
-        if(!isset($user_id,$_POST['created_by'])){
+        if(!isset($user_id,$nickname)){
             $data['code']  = 20000;
             return $data;
         }
@@ -177,7 +185,7 @@ class Images extends ActiveRecord
         $this->model     = 'avatar';
         $this->project_id= 0;
         $this->rel_id    = $user_id;
-        $this->created_by= $_POST['created_by'];
+        $this->created_by= $nickname;
         $this->o_path    = $file['path'].$file['file'];
         $this->m_path    = $img_m;
         $this->s_path    = $img_s;
@@ -192,7 +200,7 @@ class Images extends ActiveRecord
 
         //日志
         $log = new Log();
-        $log->addLog(0,0,$user_id,'user','img-upload','更新头像',$this->created_by);
+        $log->addLog(0,0,$user_id,'user','img-upload','更新头像',$nickname);
 
         //返回客户端数据
         $data['code']    = 10000;
@@ -202,7 +210,7 @@ class Images extends ActiveRecord
     }
     
     
-    public function remove($model_name='item',$id,$user_id)
+    public function remove($model_name='item',$id,$user_id,$nickname)
     {
         //验证方法
         if(!Yii::$app->request->isDelete)
@@ -214,7 +222,7 @@ class Images extends ActiveRecord
         }
 
         //检查参数是否正确
-        if(isset($params['project_id'],$id,$params['rel_id'],$model_name,$params['updated_by'])){
+        if(isset($params['project_id'],$id,$params['rel_id'],$model_name,$nickname)){
             $this->rel_id = $params['rel_id'];
             $query = $this->findOne(['model'=>$model_name,'img_id'=>$id,'project_id'=>$params['project_id'],'rel_id'=>$this->rel_id]);
             //检查记录是否存在
@@ -234,13 +242,13 @@ class Images extends ActiveRecord
         //更新关联记录
         if($model_name == 'item'){
             $model = Item::findOne(['item_id'=>$this->rel_id]);
-            $model->updated_by = $params['updated_by'];
+            $model->updated_by = $nickname;
             $model->save();
             $parent_id = $model->box_id;
             $rel_id = $model->item_id;
         }else{
             $model = Box::findOne(['box_id'=>$this->rel_id]);
-            $model->updated_by = $params['updated_by'];
+            $model->updated_by = $nickname;
             $model->save();
             $parent_id = $model->project_id;
             $rel_id = $model->box_id;
@@ -248,7 +256,7 @@ class Images extends ActiveRecord
 
         //日志
         $log = new Log();
-        $log->addLog($parent_id,$rel_id,$user_id,$model_name,'img-delete','删除图片',$params['updated_by']);
+        $log->addLog($parent_id,$rel_id,$user_id,$model_name,'img-delete','删除图片',$nickname);
 
         $data['code'] = 10000;
         return $data;
