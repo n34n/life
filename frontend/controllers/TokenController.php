0ae4c22eb96d01d7df68239a4d712661302725a2
Sam <34n@163.com>
2016-12-07 19:16:20 +0800

    frontend configure

--- frontend/controllers/TokenController.php
+++ frontend/controllers/TokenController.php
@@ -2,11 +2,14 @@
 
 namespace frontend\controllers;
 
+use common\components\WechatCallbackapiTest;
+
 class TokenController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+        $wechatObj = new WechatCallbackapiTest();
+        $wechatObj->valid();
     }
 
 }
