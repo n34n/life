4156313a505df80bc3c9977c7bb8b3cd4564cac5
Sam <34n@163.com>
2016-12-08 11:10:22 +0800

    frontend join

--- frontend/controllers/JoinController.php
+++ frontend/controllers/JoinController.php
@@ -13,8 +13,8 @@ class JoinController extends \yii\web\Controller
         $appid  = "wxdcda9da53c5d4b0b";
         $secret = "5ec3a56a7cf9a0a9ec74667a3cd05021";
         $url    = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$secret."&code=".$_GET['code']."&grant_type=authorization_code";
-        echo $rf_token_json = file($url);
-        $rf_token = json_decode($rf_token_json);
+        $rf_token_json = file($url);
+        $rf_token = json_decode($rf_token_json[0]);
         print_r($rf_token);
 
     }
