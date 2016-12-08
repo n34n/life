05c7fb51360225bdc8f400de431150b4b5283df4
Sam <34n@163.com>
2016-12-08 10:57:04 +0800

    frontend join

--- frontend/controllers/JoinController.php
+++ frontend/controllers/JoinController.php
@@ -6,7 +6,14 @@ class JoinController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+//        echo urlencode('http://m.lifeqx.com/join');
+//        echo '<br>';
+//        echo urlencode('http://m.example.com/join');
+        //return $this->render('index');
+        $appid  = "wxdcda9da53c5d4b0b";
+        $secret = "5ec3a56a7cf9a0a9ec74667a3cd05021";
+        $url    = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$secret."&code=".$_GET['code']."&grant_type=authorization_code";
+        $this->redirect($url);
     }
 
 }
