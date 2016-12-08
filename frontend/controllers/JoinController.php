feac416a56913ce9af22e48223f7a919dc28ee09
Sam <34n@163.com>
2016-12-08 11:40:50 +0800

    frontend join

--- frontend/controllers/JoinController.php
+++ frontend/controllers/JoinController.php
@@ -12,10 +12,21 @@ class JoinController extends \yii\web\Controller
         //return $this->render('index');
         $appid  = "wxdcda9da53c5d4b0b";
         $secret = "5ec3a56a7cf9a0a9ec74667a3cd05021";
+
+        //获取access_token和openid
         $url    = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$appid."&secret=".$secret."&code=".$_GET['code']."&grant_type=authorization_code";
-        $rf_token_json = file($url);
-        $rf_token = json_decode($rf_token_json[0]);
-        print_r($rf_token);
+        $token_json = file($url);
+        $token = json_decode($token_json[0]);
+
+        //获取用户数据
+        if(isset($token) && !empty($token->access_token)){
+            $access_token   = $token->access_token;
+            $openid         = $token->openid;
+            $url = "https://api.weixin.qq.com/sns/userinfo?access_token=".$access_token."&openid=".$openid."&lang=zh_CN";
+            $userinfo_json = file($url);
+            $userinfo = json_decode($userinfo_json[0]);
+            print_r($userinfo);
+        }
 
     }
 
