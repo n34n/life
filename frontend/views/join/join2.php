b1d07558103ffe415b00b3f3eedffe79ea13009c
Sam <34n@163.com>
2016-12-12 18:09:08 +0800

    system maintenance

--- frontend/views/join/join2.php
+++ frontend/views/join/join2.php
@@ -24,17 +24,60 @@ $this->title = '物语千寻';
     <a href="com.baidu.tieba://">com.baidu.tieba</a>
     <a href="https://itunes.apple.com/cn/app/id477927812" id="openApp">贴吧客户端</a>
     <script type="text/javascript">
-        document.getElementById('openApp').onclick = function(e){
-            // 通过iframe的方式试图打开APP，如果能正常打开，会直接切换到APP，并自动阻止a标签的默认行为
-            // 否则打开a标签的href链接
+
+        if(navigator.userAgent.match(/(iPhone|iPod|iPad);?/i))  {
+
+            //Animation://com.yz.animation
+
+            var isInstalled;
+
+            //var gz = '{"comName":"${com.short_name}","comID":"${com.id}","comPhoneNum":"${com.phone_num}","type":"0"}';
+
+            //var jsongz =JSON.parse(gz);
+
+
+
+            //下面是IOS调用的地址，自己根据情况去修改
+
+            var ifrSrc = 'com.baidu.tieba://';
+
             var ifr = document.createElement('iframe');
-            ifr.src = 'com.baidu.tieba://';
+
+            ifr.src = ifrSrc;
+
             ifr.style.display = 'none';
+
+            ifr.onload = function() {
+
+                // alert('Is installed.');
+
+                isInstalled = true;
+
+                alert(isInstalled);
+
+                document.getElementById('openApp').click();};
+
+            ifr.onerror = function() {
+
+                // alert('May be not installed.');
+
+                isInstalled = false;
+
+                alert(isInstalled);
+
+            }
+
             document.body.appendChild(ifr);
-            window.setTimeout(function(){
+
+            setTimeout(function() {
+
                 document.body.removeChild(ifr);
-            },3000)
-        };
+
+            },1000);
+
+        }
+
+        }
     </script>
 </div>
 
