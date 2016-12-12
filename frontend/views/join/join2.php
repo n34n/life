da8a26c2c732b238914070d91ef648aa79fe974f
Sam <34n@163.com>
2016-12-12 17:16:05 +0800

    system maintenance

--- frontend/views/join/join2.php
+++ frontend/views/join/join2.php
@@ -19,7 +19,7 @@ $this->title = '物语千寻';
 </div>
 
 <div class="dataform">
-    <button class="button" onClick="location.href='jump'">进入物语千寻</button>
+    <button class="button" onClick="location.href='/join/jump'">进入物语千寻</button>
     <a href="join/jump">download</a>
 </div>
 
