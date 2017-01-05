b4d8fd27e9956c47957b0fdd91adf1dfa3bbd6f9
Sam <34n@163.com>
2017-01-05 17:45:22 +0800

    remove image fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -15,6 +15,8 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" id="viewport" name="viewport">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="shortcut icon" type="image/x-icon" href="http://www.lifeqx.com/images/favicon.ico" media="screen" />
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
