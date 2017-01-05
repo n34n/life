78a2e2cabf9241353becef3e30c2552dd5e4dbc0
Sam <34n@163.com>
2017-01-05 17:51:29 +0800

    favicon update

--- frontend/views/layouts/web.php
+++ frontend/views/layouts/web.php
@@ -16,7 +16,7 @@ WebAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" id="viewport" name="viewport">
     <meta name="format-detection" content="telephone=no">
-    <link rel="shortcut icon" type="image/x-icon" href="http://www.lifeqx.com/images/favicon.ico" media="screen" />
+    <link rel="shortcut icon" type="image/x-icon" href="http://www.lifeqx.com/images/favicon2.ico" media="screen" />
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
